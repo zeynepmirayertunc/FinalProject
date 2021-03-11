eb3583fd727a445ba2bf1ff733df9dbf2de2d6dd
zeynepmirayertunc <69868488+zeynepmirayertunc@users.noreply.github.com>
2021-03-11 21:39:27 +0300

    11 Mart/March 11

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -38,8 +38,8 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            
-           // services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            // services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -70,6 +70,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
